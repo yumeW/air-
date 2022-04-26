48956e7473efd7920481eb6e494474e5dc825095
yumeW <73170997+yumeW@users.noreply.github.com>
2022-04-26 16:40:34 +0100

    environment port refined

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fetch = require('node-fetch');
 
 
 const app = express();
-const port = port.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('listening at 3000'));
 app.use(express.static('public'));
